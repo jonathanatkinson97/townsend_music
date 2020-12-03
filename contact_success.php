85c0beaa9fe1b4c310a51008baf9919ec513be76
Jonathan Atkinson <jonathan.atkinson97@gmail.com>
2020-12-03 22:29:04 +0000

    Implement basic styling

---
+++ contact_success.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Message Sent!</title>
+        <link rel="stylesheet" href="stylesheet.css">
+    <head>
+
+    <body>
+        <div class="content-wrapper">
+            <h1>Contact Us</h1>
+
+            <p>Message Sent! We'll be in touch with you shortly.</p>
+
+            <a href="index.php">Home</a>
+
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -20,6 +20,8 @@ if(isset($_POST['submitted'])) {
         $message = new Message;
         $message->create($_POST, $user->getId());
         $message->mail();
+
+        header('Location: contact_success.php');
     }
 }
 
@@ -28,48 +30,54 @@ if(isset($_POST['submitted'])) {
 
 <html>
     <head>
+        <link rel="stylesheet" href="stylesheet.css">
     </head>
 
     <body>
 
-        <h1>Contact Us</h1>
+        <div class="content-wrapper">
+            <h1>Contact Us</h1>
 
-        <form action="/townsend_music/index.php" method="POST">
+            <div class="form-wrapper">
 
-            <input type="hidden" name="submitted" value="true"/>
+                <form action="/townsend_music/index.php" method="POST">
 
-            <div class="input-field">
-                <label for="name">Name</label>
-                <input id="name" type="text" name="name" value="<?= $_POST['name'] ?? '' ?>" required />
-                <p class="error"><?= $errors['name'] ?? '' ?></p>
+                    <input type="hidden" name="submitted" value="true"/>
+
+                    <div class="input-field">
+                        <label for="name">Name</label>
+                        <input id="name" type="text" name="name" value="<?= $_POST['name'] ?? '' ?>" required />
+                        <p class="error"><?= $errors['name'] ?? '' ?></p>
+                    </div>
+
+                    <div class="input-field">
+                        <label for="phone">Phone</label>
+                        <input id="phone" type="text" name="phone" value="<?= $_POST['phone'] ?? '' ?>" pattern="^[\s0-9]{10,}$" required />
+                        <p class="error"><?= $errors['phone'] ?? '' ?></p>
+                    </div>
+
+                    <div class="input-field">
+                        <label for="email">Email</label>
+                        <input id="email" type="email" name="email" value="<?= $_POST['email'] ?? '' ?>" required />
+                        <p class="error"><?= $errors['email'] ?? '' ?></p>
+                    </div>
+
+                    <div class="input-field">
+                        <label for="message">Message</label>
+                        <textarea id="message" name="message" value="<?= $_POST['message'] ?? '' ?>" required></textarea>
+                        <p class="error"><?= $errors['message'] ?? '' ?></p>
+                    </div>
+
+                    <div class="input-field">
+                        <label for="subscribed">Subscribe?</label>
+                        <input id="subscribed" type="checkbox" name="subscribed" checked="<?= $_POST['subscribed'] === 'true' ?>" value="1" />
+                    </div>
+
+                    <input id="submit" type="submit" />
+
+                </form>
             </div>
-
-            <div class="input-field">
-                <label for="phone">Phone</label>
-                <input id="phone" type="text" name="phone" value="<?= $_POST['phone'] ?? '' ?>" pattern="^[\s0-9]{10,}$" required />
-                <p class="error"><?= $errors['phone'] ?? '' ?></p>
-            </div>
-
-            <div class="input-field">
-                <label for="email">Email</label>
-                <input id="email" type="email" name="email" value="<?= $_POST['email'] ?? '' ?>" required />
-                <p class="error"><?= $errors['email'] ?? '' ?></p>
-            </div>
-
-            <div class="input-field">
-                <label for="message">Message</label>
-                <textarea id="message" name="message" value="<?= $_POST['message'] ?? '' ?>" required></textarea>
-                <p class="error"><?= $errors['message'] ?? '' ?></p>
-            </div>
-
-            <div class="input-field">
-                <label for="subscribed">Subscribe?</label>
-                <input id="subscribed" type="checkbox" name="subscribed" checked="<?= $_POST['subscribed'] === 'true' ?>" value="1" />
-            </div>
-
-            <input type="submit" />
-
-        </form>
+        </div>
 
     </body>
 </html>
